d0d2de947b8cb905bef52e7c4cdbd52c868baf23
YI RONG <yrong9@info-hsg-095-x.ad.wisc.edu>
2016-09-06 12:18:47 -0500

    remove brew

--- Scripts/setitup.sh
+++ Scripts/setitup.sh
@@ -1,12 +1,12 @@
 #~/usr/bin/env bash
-echo 'Preparing HomeBrew'
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-echo 'Downloading Telegram'
-brew install caskroom/cask/brew-cask 2> /dev/null
-brew cask install telegram
-echo 'Downloading Vim'
-brew tap macvim-dev/macvim
-brew install --HEAD macvim-dev/macvim/macvim
+mkdir -p ~/LER0ever
+cd ~/LER0ever
+echo ‘Downloading MacVim DMG’
+curl -LOk -o macvim.dmg https://github.com/macvim-dev/macvim/releases/download/snapshot-107/MacVim.dmg
+hdiutil attach ~/LER0ever/macvim.dmg
+echo ‘Downloading Telegram DMG’
+curl -LOk -o telegram.dmg https://updates.tdesktop.com/tmac/tsetup.0.10.1.dmg
+hdiutil attach ~/LER0ever/telegram.dmg
 echo 'Fetching Config from LER0ever EverVim'
 git clone https://github.com/LER0ever/EverVim
 echo 'Setting up EverVim'
